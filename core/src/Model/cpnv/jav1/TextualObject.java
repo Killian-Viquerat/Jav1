7596e4c0f09be969668ba9bfe0358d029e44b5df
Killian Viquerat <killian.viquerat@cpnv.ch>
2020-11-12 11:02:11 +0100

    Change textual class to follow uml

--- core/src/Model/cpnv/jav1/TextualObject.java
+++ core/src/Model/cpnv/jav1/TextualObject.java
@@ -4,15 +4,15 @@ import com.badlogic.gdx.graphics.Texture;
 
 import Model.cpnv.jav1.data.Word;
 
-public class TextualObject extends PhysicalObject {
-    protected Word word;
+public abstract class TextualObject extends PhysicalObject {
+    protected String text;
 
-    public TextualObject(String picName, float srcX, float srcY,  int WIDTH, int HEIGHT, Word word) {
+    public TextualObject(String picName, float srcX, float srcY,  int WIDTH, int HEIGHT, String word) {
         super(picName, srcX, srcY, WIDTH, HEIGHT);
-        this.word = word;
+        this.text = word;
     }
 
-    protected Word getWord(){
-        return word;
+    protected String getWord(){
+        return text;
     }
 }
